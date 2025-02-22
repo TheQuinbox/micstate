761b6ea1202b30e24966e14ebc58eab215f953d5
Quin <TheQuinbox@gmail.com>
2025-02-22 10:50:29 -0700

    Update readme and make it build and link once again.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Micstate
-
 Simple C++ program to speak the muted state of your default input device using a global hotkey.
 
 ## Why?
-
 The current way of determining if an input device is muted or not on Windows is incredibly annoying:
 
 1. Open mmsys.cpl.
@@ -19,15 +17,4 @@ The current way of determining if an input device is muted or not on Windows is 
 I have a laptop with a deticated mute key that's super easy to hit, and I've hit it by accident many times, initially leading to my great confusion when no one could hear me, and later my paranoia that I hit it every time I launched a program that used the microphone. Hence, this program was born. Once you run it, it will passively sit in the background, waiting for you to press Control+Alt+Shift+M. When you do, the current state of your default input device will be spoken.
 
 ## Configuration
-
-The way this program works is by taking a tiny sample of audio (50 MS or so) from your default input device, getting the noise level (in DB), and comparing it to a configurable value. If the value is less than or equal to the configured value, the microphone is considered muted. By default, the value is -60 DB, but you can always change it. Open config.ini (generated when you run the program for the first time), and change the value of min_level in the DEFAULT section to your desired value. You'll have to relaunch the program for this change to take effect.
-
-## Building from Source
-
-I'm still very new to CMake, and even more so to needing to make my builds easily reproducible. As such, the current CMakeLists in this repository would probably give any CMake maintainer a heart attack. The external dependencies you'll need are:
-
-1. [Tolk](https://github.com/dkager/Tolk)
-2. [Bass](https://www.un4seen.com/bass.html)
-3. [SimpleINI](https://github.com/brofield/simpleini)
-
-The only lines you should need to change in the CMakeLists file are the `target_include_directories`, `target_link_directories`, and  `file` calls to point to wherever you have the required headers, lib files, and DLLs, respectively. I keep mine in C:\dev\include, C:\dev\lib, and C:\dev\bin, but it's completely up to you where you put them, as long as you remember to update the paths. I appologise for this incredibly kludgy setup, and hope to improve it at some point in the future. This is one area where I am very actively open to suggestions.
+The way this program works is by taking a tiny sample of audio (50 MS or so) from your default input device, getting the noise level (in DB), and comparing it to a configurable value. If the value is less than or equal to the configured value, the microphone is considered muted. By default, the value is -60 DB, but you can always change it. Open the config.ini file alongside the program and change the value of MinLevel in the Settings section to your desired value. You'll have to relaunch the program for this change to take effect.
--- micstate.c
+++ micstate.c
@@ -4,67 +4,60 @@
 #include <bass.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <shlwapi.h>
 
-#define ID_TRAYICON 101
-#define IDM_EXIT 102
-#define IDH_CHECKHOTKEY 103
+#define ID_TRAY_ICON 101
+#define ID_EXIT 102
+#define ID_CHECK_HOTKEY 103
 
 int min_level; // The minimum level (in dB) to indicate muted state.
 
-bool LoadConfig();
-void CheckMicState();
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-float LinearToDecibel(float linear);
-void ShowTrayIcon(HWND hWnd, UINT uID, HICON hIcon, LPCSTR szToolTip);
-void DestroyTrayIcon(HWND hWnd, UINT uID);
-void ShowPopupMenu(HWND hWnd);
+bool load_config();
+void check_mic_state();
+LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+float linear_to_decibel(float linear);
+void show_tray_icon(HWND hwnd, UINT id, HICON icon, LPCSTR message);
+void destroy_tray_icon(HWND hwnd, UINT id);
+void show_popup_menu(HWND hwnd);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow) {
 	if (!BASS_RecordInit(-1)) {
 		MessageBox(NULL, "Failed to initialize BASS.", "Error", MB_ICONERROR);
 		return 1;
 	}
-	if (!LoadConfig()) {
+	if (!load_config()) {
 		MessageBox(NULL, "Couldn't load configuration.", "Error", MB_ICONERROR);
 		return 1;
 	}
 	WNDCLASS wc = {0};
-	wc.lpfnWndProc = WndProc;
+	wc.lpfnWndProc = wnd_proc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = "MicStateWNDClass";
 	RegisterClass(&wc);
 	HWND hwnd = CreateWindow(wc.lpszClassName, NULL, 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
 	HICON icon = LoadIcon(NULL, IDI_APPLICATION);
-	ShowTrayIcon(hwnd, ID_TRAYICON, icon, "MicState");
-	MSG msg;
+	show_tray_icon(hwnd, ID_TRAY_ICON, icon, "MicState");
+	MSG msg = {0};;
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	UnregisterHotKey(hwnd, IDH_CHECKHOTKEY);
-	DestroyTrayIcon(hwnd, ID_TRAYICON);
+	UnregisterHotKey(hwnd, ID_CHECK_HOTKEY);
+	destroy_tray_icon(hwnd, ID_TRAY_ICON);
 	return 0;
 }
 
-bool LoadConfig() {
-	const char* filename = "config.ini";
-	const char* section = "DEFAULT";
-	const char* key = "min_level";
-	char buffer[16];
-	int min_level_default = -60;
-	DWORD charsRead = GetPrivateProfileString(section, key, NULL, buffer, sizeof(buffer), filename);
-	if (charsRead == 0) {
-		char defaultValue[16];
-		snprintf(defaultValue, sizeof(defaultValue), "%d", min_level_default);
-		WritePrivateProfileString(section, key, defaultValue, filename);
-		charsRead = GetPrivateProfileString(section, key, NULL, buffer, sizeof(buffer), filename);
-		if (charsRead == 0) return false;
-	}
-	min_level = atoi(buffer);
+bool load_config() {
+	char min_level_str[16] = {0};
+	char config_path[MAX_PATH] = {0};
+	if (GetCurrentDirectory(sizeof(config_path), config_path) == 0) return false;
+	if (!PathCombine(config_path, config_path, "config.ini")) return false;
+	if (GetPrivateProfileString("settings", "min_level", NULL, min_level_str, sizeof(min_level_str), config_path) == 0) return false;
+	min_level = atoi(min_level_str);
 	return true;
 }
 
-void CheckMicState() {
+void check_mic_state() {
 	HRECORD rec = BASS_RecordStart(0, 0, 0, NULL, NULL);
 	if (rec == 0) {
 		Beep(100, 100);
@@ -74,74 +67,74 @@ void CheckMicState() {
 	float levels[2];
 	BASS_ChannelGetLevelEx(rec, levels, 1.0, BASS_LEVEL_MONO);
 	BASS_ChannelStop(rec);
-	int level = (int)LinearToDecibel(levels[0]);
+	int level = (int)linear_to_decibel(levels[0]);
 	speechSay(level <= min_level ? L"Muted" : L"Unmuted", TRUE);
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	switch (uMsg) {
+LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
 	case WM_CREATE:
-		if (!RegisterHotKey(hWnd, IDH_CHECKHOTKEY, MOD_CONTROL | MOD_ALT | MOD_SHIFT, 'M')) {
+		if (!RegisterHotKey(hwnd, ID_CHECK_HOTKEY, MOD_CONTROL | MOD_ALT | MOD_SHIFT, 'M')) {
 			MessageBox(NULL, "Failed to register hotkey!", "Error", MB_ICONERROR | MB_OK);
 			return -1;
 		}
 		break;
 	case WM_HOTKEY:
-		if (wParam == IDH_CHECKHOTKEY) CheckMicState();
+		if (wp == ID_CHECK_HOTKEY) check_mic_state();
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
-	case ID_TRAYICON:
-		switch (lParam) {
+	case ID_TRAY_ICON:
+		switch (lp) {
 		case WM_LBUTTONDBLCLK:
 		case WM_LBUTTONDOWN:
 		case WM_RBUTTONDBLCLK:
 		case WM_RBUTTONDOWN:
-			ShowPopupMenu(hWnd);
+			show_popup_menu(hwnd);
 			break;
 		}
 		break;
 	case WM_COMMAND:
-		if (LOWORD(wParam) == IDM_EXIT) PostQuitMessage(0);
+		if (LOWORD(wp) == ID_EXIT) PostQuitMessage(0);
 		break;
 	default:
-		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+		return DefWindowProc(hwnd, msg, wp, lp);
 	}
 	return 0;
 }
 
-float LinearToDecibel(float linear) {
+float linear_to_decibel(float linear) {
 	if (linear != 0.0f) return 20.0f * log10f(linear);
 	return -144.0f;  // Effectively minus infinity
 }
 
-void ShowTrayIcon(HWND hWnd, UINT uID, HICON hIcon, PCSTR szToolTip) {
+void show_tray_icon(HWND hwnd, UINT id, HICON icon, PCSTR message) {
 	NOTIFYICONDATA nd = {0};
 	nd.cbSize = sizeof(NOTIFYICONDATA);
-	nd.hWnd = hWnd;
-	nd.uID = uID;
+	nd.hWnd = hwnd;
+	nd.uID = id;
 	nd.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-	nd.uCallbackMessage = ID_TRAYICON;
-	nd.hIcon = hIcon;
-	strncpy(nd.szTip, szToolTip, sizeof(nd.szTip) - 1);
+	nd.uCallbackMessage = ID_TRAY_ICON;
+	nd.hIcon = icon;
+	strncpy(nd.szTip, message, sizeof(nd.szTip) - 1);
 	Shell_NotifyIcon(NIM_ADD, &nd);
 }
 
-void DestroyTrayIcon(HWND hWnd, UINT uID) {
-	NOTIFYICONDATA ndata = {0};
-	ndata.cbSize = sizeof(NOTIFYICONDATA);
-	ndata.hWnd = hWnd;
-	ndata.uID = uID;
-	Shell_NotifyIcon(NIM_DELETE, &ndata);
+void destroy_tray_icon(HWND hwnd, UINT id) {
+	NOTIFYICONDATA nd = {0};
+	nd.cbSize = sizeof(NOTIFYICONDATA);
+	nd.hWnd = hwnd;
+	nd.uID = id;
+	Shell_NotifyIcon(NIM_DELETE, &nd);
 }
 
-void ShowPopupMenu(HWND hWnd) {
+void show_popup_menu(HWND hwnd) {
 	HMENU menu = CreatePopupMenu();
-	AppendMenu(menu, MF_STRING, IDM_EXIT, "E&xit");
+	AppendMenu(menu, MF_STRING, ID_EXIT, "E&xit");
 	POINT cursor;
 	GetCursorPos(&cursor);
-	SetForegroundWindow(hWnd);
-	TrackPopupMenu(menu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, cursor.x, cursor.y, 0, hWnd, NULL);
+	SetForegroundWindow(hwnd);
+	TrackPopupMenu(menu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, cursor.x, cursor.y, 0, hwnd, NULL);
 	DestroyMenu(menu);
 }
